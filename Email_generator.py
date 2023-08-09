200b0c4b4886ff23791ba79aa12afa28ce783392
mashunia63 <mashunia63@mail.ru>
2023-08-09 11:11:07 +0300

    upgrate

--- Email_generator.py
+++ Email_generator.py
@@ -4,4 +4,7 @@ for i in range (0,10):
     email = names.get_first_name()+names.get_last_name()+"@gmail.com"
     print(email)
 
+for i in range (0,10):
+    emailSql = '(' + "'" + names.get_first_name()+names.get_last_name()+"@gmail.com" + "'" + ')'
+    print(emailSql)
 
